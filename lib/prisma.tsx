3c77972b22b05dfe58a6c168e136dfb2ddbecd6a
Cal <168704009+nfworking@users.noreply.github.com>
2026-05-26 20:50:14 +1000

    update database host in Prisma configuration

--- lib/prisma.tsx
+++ lib/prisma.tsx
@@ -2,7 +2,7 @@ import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
+  host: "172.20.0.100",
   user: "root",
   password: "ridealong",
   database: "fortmontapi",
