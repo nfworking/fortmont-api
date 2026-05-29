e19109f19939a1859daf3946a47d69dba003459d
nfworking <tidalfarmer1432@outlook.com>
2026-05-29 10:38:42 +1000

    Refactor Trial and test different backgrounds for use

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
         <img
           src="/loginbg.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[1.3] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[1.3] "
         />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { Hero115 } from "@/components/hero115";
 import { Feature17 } from "@/components/feature17";
 import { Pricing6 } from "@/components/pricing6";
+import { Footer2 } from "@/components/footer2";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-6 pt-6 md:gap-10">
-      <Hero115 />
-      <Feature17 />
-      <Pricing6 />
+    
+      <div className="flex w-full flex-col items-center gap-6 md:gap-10 ">
+        <Hero115 />
+        <Pricing6 />
+        <Feature17 />
+        <Footer2 className="w-full" />
+      </div>
 
-    </div>
-  )
+  );
 }
